4ac06f02b7502900538c400921b4be283ee933d9
Iyana <114933494+yayakixtenet@users.noreply.github.com>
2024-06-04 15:48:34 -0400

    help too much bubble logic

--- src/stories/MessageBubble.tsx
+++ src/stories/MessageBubble.tsx
@@ -5,6 +5,9 @@ import "../index.css";
 interface MessageBubbleProps {
   message: string;
   sentByMe: boolean;
+  prevMessageByMe: boolean;
+  index: number;
+  styling: string;
   onClick?: () => void;
 }
 
@@ -14,11 +17,31 @@ interface MessageBubbleProps {
 export const MessageBubble = ({
   message,
   sentByMe,
+  prevMessageByMe,
+  index,
+  styling,
   ...props
 }: MessageBubbleProps) => {
+  // is it the first message by user (in a group)
+  // is it the last message by user (in a group)
+  // is the user me or them
+
+  // everything else should be square
+  //   if (firstByUser ) round top
+  //   if (lastByUser ) round bottom
+  //   if me ? send it to the right : send it to the left (round opposite edges)
+
+  //
+  const newMessage = "rounded-tl-lg rounded-tr-lg rounded-bl-lg";
+  const middleMessage = "rounded-none";
+  const lastMessage = "rounded-br-lg rounded-bl-lg rounded-tl-lg ";
+
   return (
     <div
-      className="rounded-2xl bg-zinc-200 px-4 py-4 mt-2 w-5/6 flex flex-col "
+      className={
+        `px-4 py-4 mt-2 w-5/6 flex flex-col  ` +
+        `${index == 0 ? newMessage : " "} + ${styling}`
+      }
       style={
         sentByMe
           ? {
